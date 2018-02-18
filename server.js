929ce95f000b1442e786b3a1723d1d9e681f9364
Kyle Becker <kmbecker@users.noreply.github.com>
2018-02-18 14:00:20 -0600

    pushing additional changes

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var path = require('path');
+
+var PORT = 3000;
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || PORT;
 
-// Serve static content for the app from the public directory in the
-// application directory.
-app.use(express.static(__dirname + '/public'));
+// Serve static content for the app from public directory
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(bodyParser.json());
 
 // Parse application
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(port, function() {
-    console.log('listening on ' + port);
+
+    /* eslint no-console: "off", prefer-template: "off" */
+    console.log('listening on port ' + port);
+
 });
 
-// get routes
+// Get routes
 require('./controllers/mainController.js')(app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,25 @@
 module.exports = {
-  // This is the entry point or start of our react applicaton
-  entry: "./app/app.js",
+    // Using this code, you can debug in chrome dev tools
+    devtool: 'eval-source-map',
+    // start of react
+    entry: './app/app.js',
 
-  // The plain compiled JavaScript will be output into this file
-  output: {
-      filename: "public/bundle.js"
-  },
+    module: {
+        loaders: [
+            {
+                // Webpack will only process files in our app folder.
+                include: /app/,
+                loader: 'babel-loader',
+                query: {
+                    // These are the specific transformations we'll be using.
+                    presets: ['react', 'es2015']
+                },
+                // Only working with files that in in a .js
+                test: /\.jsx?$/
+            }
+        ]
+    },
 
-  // This section desribes the transformations we will perform
-  module: {
-      loaders: [
-          {
-              // Only working with files that are in a .js
-              test: /\.jsx?$/,
-              // Webpack will only process files in our app folder.
-              include: /app/,
-              loader: "babel-loader",
-              query: {
-                  // These are the specific transformations we'll be using.
-                  presets: ["react", "es2015"]
-              }
-          }
-      ]
-  },
-  // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
-  // Without this the console says all errors are coming from just coming from bundle.js
-  devtool: "eval-source-map"
+    // The plain compiled JavaScript will be output
+    output: {filename: 'public/bundle.js'}
 };
